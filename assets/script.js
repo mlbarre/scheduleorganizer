0ccf503ab20caf641786bb825bb62f813c59def2
mlbarre <mlbarrera1538@gmail.com>
2021-12-06 22:20:36 -0600

    added function to elements, not able to connect to localStorage

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,30 @@ var todayDate = moment().format("[Today is] dddd, MMMM Do YYYY").toString();
 document.getElementById("container").innerHTML = todayDate;
 // console.log(todayDate.format("[Today is] dddd, MMMM Do YYYY"));
 
+
+// var storageEvents = document.querySelector(".eventdes");
+// var saveButton = document.querySelector(".saveBtn");
+
+// var newEvents = function () {
+//     storageEvents.onClick(function() {
+//         var savedEvents = saveButton.val();
+//         console.log(savedEvents);
+//         // localStorage.setItem('event', savedEvents);
+//     });
+// };
+// saveButton.addEventListener("click", newEvents);
+
+
+var eventInput = document.querySelector(".eventdes");
+var saveButton = document.querySelector(".saveBtn");
+
+saveButton.addEventListener("click", function(event) {
+    event.preventDefault;
+
+    var savedEvents = {
+        newEvent: eventInput.value.trim() 
+    };
+
+    localStorage.setItem("Saved", JSON.stringify(savedEvents));
+});
+
